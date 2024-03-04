7f9a24bba26ff0e975346d8835f9be91a9833055
hantacore <hantacore@gmail.com>
2024-03-04 14:33:15 +0100

    adding missing files

---
+++ StrandedWideMod_Harmony/VersionChecker.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityModManagerNet;
+
+namespace StrandedDeepModsUtils
+{
+    public class VersionChecker
+    {
+        internal class InfoJson
+        {
+            public string Id { get; set; }
+            public string Version { get; set; }
+        }
+
+        public class Repository
+        {
+            [Serializable]
+            public class Release
+            {
+                public string Id;
+                public string Version;
+                public string DownloadUrl;
+                public string Homepage;
+            }
+
+            public Release[] Releases;
+        }
+
+        internal static void CheckVersion(UnityModManager.ModEntry currentMod, string infoJsLocation)
+        {
+            try
+            {
+                Debug.Log(currentMod.Info.DisplayName + " : checking for updates");
+                HttpWebRequest webRequest = null;
+                string strContent = "";
+
+                if (!String.IsNullOrEmpty(infoJsLocation))
+                {
+                    webRequest = (HttpWebRequest)HttpWebRequest.Create(infoJsLocation);
+                    webRequest.UserAgent = "StrandedDeepMod";
+
+                    using (var response = webRequest.GetResponse())
+                    {
+                        using (var content = response.GetResponseStream())
+                        {
+                            using (var reader = new StreamReader(content))
+                            {
+                                strContent = reader.ReadToEnd();
+                            }
+                        }
+                    }
+                    InfoJson info = TinyJson.JSONParser.FromJson<InfoJson>(strContent);
+                    Debug.Log(currentMod.Info.DisplayName + " : CheckVersion : " + info.Version);
+
+                    Version remoteVersion = Version.Parse(info.Version.Replace("-beta", ""));
+
+                    if (remoteVersion > currentMod.Version)
+                    {
+                        currentMod.CustomRequirements = "<color=orange>Update available (" + info.Version + ")</color>";
+                    }
+                    else
+                    {
+                        currentMod.CustomRequirements = "<color=green>Up to date</color>";
+                    }
+                }
+
+                // hack to avoid checking the repository for each mod
+                if (!currentMod.HasUpdate
+                    && !String.IsNullOrEmpty(currentMod.Info.Repository))
+                {
+                    Debug.Log(currentMod.Info.DisplayName + " : Repository : " + currentMod.Info.Repository);
+                    webRequest = (HttpWebRequest)HttpWebRequest.Create(currentMod.Info.Repository);
+                    webRequest.UserAgent = "StrandedDeepMod";
+                    using (var response = webRequest.GetResponse())
+                    {
+                        using (var content = response.GetResponseStream())
+                        {
+                            using (var reader = new StreamReader(content))
+                            {
+                                strContent = reader.ReadToEnd();
+                            }
+                        }
+                    }
+                    Repository repinfo = TinyJson.JSONParser.FromJson<Repository>(strContent);
+                    if (repinfo == null)
+                    {
+                        Debug.Log(currentMod.Info.DisplayName + " : could not parse repository");
+                    }
+                    else
+                    {
+                        foreach (Repository.Release release in repinfo.Releases)
+                        {
+                            try
+                            {
+                                //Debug.Log(currentMod.Info.DisplayName + " : looking for " + release.Id);
+                                UnityModManager.ModEntry modEntry = UnityModManager.FindMod(release.Id);
+                                if (modEntry != null)
+                                {
+                                    // hack to avoid checking the repository for each mod
+                                    modEntry.HasUpdate = true;
+
+                                    Debug.Log(currentMod.Info.DisplayName + " : repository advertised mod version  " + release.Id + " / " + release.Version);
+                                    if (string.IsNullOrEmpty(modEntry.CustomRequirements))
+                                    {
+                                        Version remoteVersionRepo = Version.Parse(release.Version.Replace("-beta", ""));
+                                        //Debug.Log(currentMod.Info.DisplayName + " : remoteVersionRepo " + remoteVersionRepo);
+                                        Version localVersion = Version.Parse(modEntry.Info.Version.Replace("-beta", ""));
+                                        //Debug.Log(currentMod.Info.DisplayName + " : localVersion " + localVersion);
+                                        if (remoteVersionRepo > localVersion)
+                                        {
+                                            modEntry.CustomRequirements = "<color=orange>Update available (" + release.Version + ")</color>";
+                                        }
+                                        else
+                                        {
+                                            modEntry.CustomRequirements = "<color=green>Up to date</color>";
+                                        }
+                                    }
+                                    if (string.IsNullOrEmpty(modEntry.Info.HomePage)
+                                        && !string.IsNullOrEmpty(release.Homepage))
+                                    {
+                                        modEntry.Info.HomePage = release.Homepage;
+                                    }
+                                }
+                                else
+                                {
+                                    Debug.Log(currentMod.Info.DisplayName + " : " + release.Id + " not installed");
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                Debug.Log(currentMod.Info.DisplayName + " : error while handling modEntry : " + ex);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                currentMod.CustomRequirements = "Could not check for updates";
+                Debug.Log(currentMod.Info.DisplayName + " : error on CheckVersion : " + e);
+            }
+        }
+    }
+}
---
+++ StrandedWideMod_Harmony/WorldUtilities.cs
@@ -0,0 +1,322 @@
+﻿using Beam;
+using Beam.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using UnityModManagerNet;
+
+namespace StrandedDeepModsUtils
+{
+    public static class WorldUtilities
+    {
+        private static bool worldLoaded = false;
+        private static StrandedWorld previousInstance = null;
+
+        private static UnityModManager.ModEntry mewide = null;
+        private static PropertyInfo pi_islandSize = null;
+        private static PropertyInfo pi_islandSizeRatio = null;
+        private static PropertyInfo pi_zoneSize = null;
+        private static PropertyInfo pi_zoneSpacing = null;
+        private static PropertyInfo pi_islandsCount = null;
+        private static PropertyInfo pi_overlayPosition = null;
+        private static Type strandedWideMainType = null;
+
+        public static bool IsStrandedWide()
+        {
+            mewide = UnityModManager.FindMod("StrandedWideMod");
+            return (mewide != null && mewide.Active && mewide.Loaded);
+        }
+
+        public static int IslandSize
+        {
+            get
+            {
+                if (IsStrandedWide())
+                {
+                    if (pi_islandSize == null)
+                    {
+                        if (strandedWideMainType == null)
+                        {
+                            strandedWideMainType = mewide.Assembly.GetType("StrandedWideMod_Harmony.Main");
+                        }
+                        if (strandedWideMainType != null)
+                        {
+                            pi_islandSize = strandedWideMainType.GetProperty("IslandSize", BindingFlags.Static | BindingFlags.Public);
+                        }
+                        else
+                        {
+                            Debug.Log("Stranded Deep World Utilities : Stranded Wide type null");
+                        }
+                    }
+                    if (pi_islandSize != null)
+                    {
+                        int swideIslandSize = (int)pi_islandSize.GetValue(null);
+                        //Debug.Log("Stranded Deep World Utilities : Stranded Wide island size retrieved : " + swideIslandSize);
+                        return swideIslandSize;
+                    }
+                    else
+                    {
+                        Debug.Log("Stranded Deep World Utilities : Stranded Wide pi_islandSize null");
+                        return 512;
+                    }
+                }
+
+                return StrandedWorld.ZONE_HEIGHTMAP_SIZE - 1;
+            }
+        }
+
+        public static float ZoneTerrainSize
+        {
+            get
+            {
+                return IslandSize - IslandSizeRatio * 6;//500f//250f;
+            }
+        }
+
+        public static int IslandSizeRatio
+        {
+            get
+            {
+                if (IsStrandedWide())
+                {
+                    if (pi_islandSizeRatio == null)
+                    {
+                        if (strandedWideMainType == null)
+                        {
+                            strandedWideMainType = mewide.Assembly.GetType("StrandedWideMod_Harmony.Main");
+                        }
+                        if (strandedWideMainType != null)
+                        {
+                            pi_islandSizeRatio = strandedWideMainType.GetProperty("IslandSizeRatio", BindingFlags.Static | BindingFlags.Public);
+                        }
+                        else
+                        {
+                            Debug.Log("Stranded Deep World Utilities : Stranded Wide type null");
+                        }
+                    }
+                    if (pi_islandSizeRatio != null)
+                    {
+                        int swideIslandSizeRatio = (int)pi_islandSizeRatio.GetValue(null);
+                        //Debug.Log("Stranded Deep World Utilities : Stranded Wide island size ratio retrieved : " + swideIslandSizeRatio);
+                        return swideIslandSizeRatio;
+                    }
+                    else
+                    {
+                        Debug.Log("Stranded Deep World Utilities : Stranded Wide pi_islandSizeRatio null");
+                        return 2;
+                    }
+                }
+
+                return 1;
+            }
+        }
+
+        public static float ZoneSize
+        {
+            get
+            {
+                if (IsStrandedWide())
+                {
+                    if (pi_zoneSize == null)
+                    {
+                        if (strandedWideMainType == null)
+                        {
+                            strandedWideMainType = mewide.Assembly.GetType("StrandedWideMod_Harmony.Main");
+                        }
+                        if (strandedWideMainType != null)
+                        {
+                            pi_zoneSize = strandedWideMainType.GetProperty("ZoneSize", BindingFlags.Static | BindingFlags.Public);
+                        }
+                        else
+                        {
+                            Debug.Log("Stranded Deep World Utilities : Stranded Wide type null");
+                        }
+                    }
+                    if (pi_zoneSize != null)
+                    {
+                        float swideZoneSize = (float)pi_zoneSize.GetValue(null);
+                        //Debug.Log("Stranded Deep World Utilities : Stranded Wide zone size retrieved : " + swideZoneSize);
+                        return swideZoneSize;
+                    }
+                    else
+                    {
+                        Debug.Log("Stranded Deep World Utilities : Stranded Wide pi_zoneSize null");
+                        return 1000;
+                    }
+                }
+
+                return StrandedWorld.ZONE_SIZE;
+            }
+        }
+
+        public static float ZoneSpacing
+        {
+            get
+            {
+                if (IsStrandedWide())
+                {
+                    if (pi_zoneSpacing == null)
+                    {
+                        if (strandedWideMainType == null)
+                        {
+                            strandedWideMainType = mewide.Assembly.GetType("StrandedWideMod_Harmony.Main");
+                        }
+                        if (strandedWideMainType != null)
+                        {
+                            pi_zoneSpacing = strandedWideMainType.GetProperty("ZoneSpacing", BindingFlags.Static | BindingFlags.Public);
+                        }
+                        else
+                        {
+                            Debug.Log("Stranded Deep World Utilities : Stranded Wide type null");
+                        }
+                    }
+                    if (pi_zoneSpacing != null)
+                    {
+                        float swideZoneSpacing = (float)pi_zoneSpacing.GetValue(null);
+                        //Debug.Log("Stranded Deep World Utilities : Stranded Wide zone spacing retrieved : " + swideZoneSpacing);
+                        return swideZoneSpacing;
+                    }
+                    else
+                    {
+                        Debug.Log("Stranded Deep World Utilities : Stranded Wide pi_zoneSpacing null");
+                        return 1.25f;
+                    }
+                }
+
+                return StrandedWorld.ZONE_SPACING;
+            }
+        }
+
+
+        public static int IslandsCount
+        {
+            get
+            {
+                if (IsStrandedWide())
+                {
+                    if (pi_islandsCount == null)
+                    {
+                        if (strandedWideMainType == null)
+                        {
+                            strandedWideMainType = mewide.Assembly.GetType("StrandedWideMod_Harmony.Main");
+                        }
+                        if (strandedWideMainType != null)
+                        {
+                            pi_islandsCount = strandedWideMainType.GetProperty("IslandsCount", BindingFlags.Static | BindingFlags.Public);
+                        }
+                        else
+                        {
+                            Debug.Log("Stranded Deep World Utilities : Stranded Wide type null");
+                        }
+                    }
+                    if (pi_islandsCount != null)
+                    {
+                        int swideIslandCount = (int)pi_islandsCount.GetValue(null);
+                        //Debug.Log("Stranded Deep World Utilities : Stranded Wide island count retrieved : " + swideIslandCount);
+                        return swideIslandCount;
+                    }
+                    else
+                    {
+                        Debug.Log("Stranded Deep World Utilities : Stranded Wide pi_islandsCount null");
+                        return 49;
+                    }
+                }
+
+                return StrandedWorld.WORLD_ZONES_SQUARED;
+            }
+        }
+
+        public static float WaveOverlayPosition
+        {
+            get
+            {
+                if (IsStrandedWide())
+                {
+                    if (pi_overlayPosition == null)
+                    {
+                        if (strandedWideMainType == null)
+                        {
+                            strandedWideMainType = mewide.Assembly.GetType("StrandedWideMod_Harmony.Main");
+                        }
+                        if (strandedWideMainType != null)
+                        {
+                            pi_overlayPosition = strandedWideMainType.GetProperty("WaveOverlayPosition", BindingFlags.Static | BindingFlags.Public);
+                        }
+                        else
+                        {
+                            Debug.Log("Stranded Deep World Utilities : Stranded Wide type null");
+                        }
+                    }
+                    if (pi_overlayPosition != null)
+                    {
+                        float waveOverlayPosition = (float)pi_overlayPosition.GetValue(null);
+                        Debug.Log("Stranded Deep World Utilities : Stranded Wide overlay position retrieved : " + waveOverlayPosition);
+                        return waveOverlayPosition;
+                    }
+                    else
+                    {
+                        Debug.Log("Stranded Deep World Utilities : Stranded Wide pi_islandsCount null");
+                        return 110f;
+                    }
+                }
+
+                return StrandedWorld.WORLD_ZONES_SQUARED;
+            }
+        }
+
+        public static int MaxPlayers
+        {
+            get
+            {
+                return 2;
+            }
+        }
+
+        public static bool IsWorldLoaded()
+        {
+            if (Beam.Game.State == GameState.NEW_GAME
+                || Beam.Game.State == GameState.LOAD_GAME)
+            {
+                // anti memory leak
+                if (previousInstance != null
+                && !System.Object.ReferenceEquals(previousInstance, StrandedWorld.Instance))
+                {
+                    Debug.Log("Stranded Deep World Utilities : world instance changed, clearing events");
+                    previousInstance.WorldGenerated -= Instance_WorldGenerated;
+                    previousInstance = null;
+                    worldLoaded = false;
+                }
+
+                if (StrandedWorld.Instance != null
+                    && !System.Object.ReferenceEquals(StrandedWorld.Instance, previousInstance))
+                {
+                    Debug.Log("Stranded Deep World Utilities : world instance found, registering events");
+                    previousInstance = StrandedWorld.Instance;
+                    StrandedWorld.Instance.WorldGenerated -= Instance_WorldGenerated;
+                    StrandedWorld.Instance.WorldGenerated += Instance_WorldGenerated;
+                    worldLoaded = false;
+                }
+            }
+            else
+            {
+                Reset();
+            }
+
+            return worldLoaded;
+        }
+
+        private static void Reset()
+        {
+            worldLoaded = false;
+        }
+
+        private static void Instance_WorldGenerated()
+        {
+            Debug.Log("Stranded Deep World Utilities : World Loaded event");
+            worldLoaded = true;
+        }
+    }
+}
