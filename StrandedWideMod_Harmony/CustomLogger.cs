1cc215e283e329dbe03df4cf14c0edfebc64bb1b
hantacore <hantacore@gmail.com>
2025-07-11 15:34:26 +0200

    0.1.9 customlogger fix

--- StrandedWideMod_Harmony/CustomLogger.cs
+++ StrandedWideMod_Harmony/CustomLogger.cs
@@ -15,21 +15,40 @@ namespace StrandedWideMod_Harmony
 
         public static void InitCustomLogger(string directory)
         {
-            filePath = Path.Combine(directory, "StrandedWideLog.log");
-
-            Debug.Log("Stranded Wide (Harmony edition) : placing custom log file in : " + filePath);
-
-            if (File.Exists(filePath))
+            try
             {
-                File.Delete (filePath);
+                if (!Directory.Exists(directory))
+                {
+                    Debug.Log("Stranded Wide (Harmony edition) : creating directory : " + directory);
+                    Directory.CreateDirectory(directory);
+                }
+
+                filePath = Path.Combine(directory, "StrandedWideLog.log");
+
+                Debug.Log("Stranded Wide (Harmony edition) : placing custom log file in : " + filePath);
+
+                if (File.Exists(filePath))
+                {
+                    File.Delete(filePath);
+                }
+                logTarget = File.AppendText(filePath);
             }
-            logTarget = File.AppendText(filePath);
+            catch (Exception e)
+            {
+                Debug.Log("Stranded Wide (Harmony edition) : error while initializing custom log : " + e);
+            }
         }
 
         internal static void Log(string message)
         {
             try
             {
+                if (!Directory.Exists(Path.GetDirectoryName(filePath)))
+                {
+                    Debug.Log("Stranded Wide (Harmony edition) : creating directory : " + directory);
+                    Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+                }
+
                 if (!File.Exists(filePath))
                 {
                     File.Create(filePath);
